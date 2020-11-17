b42e07c7ed487e53b1e1c92c3de2f349d4da8890
kaylyntorres <kaylyn.torres16@gmail.com>
2020-11-17 17:30:27 -0500

    Create model and constructor for feedback table

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
+
 # Rotue for form / homepage
 @app.route('/')
 def index():
