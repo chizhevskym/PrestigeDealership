9a1d6b16d093ee1041ba7a100ecfd396f9c2f311
kaylyntorres <kaylyn.torres16@gmail.com>
2020-11-16 20:48:39 -0500

    Update app.py
    Add route for submit, verify form data appears

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
+
+# Rotue for form / homepage
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# verify method is post
+# return form data as variables
+#print data in console
+#render sucess page if true
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        print(customer, dealer, rating, comments)
+        return render_template('success.html')
 
 if __name__ == '__main__':
     app.debug = True
